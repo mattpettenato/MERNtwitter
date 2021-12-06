32ee707ccb051094146c29a4728b0433459e9b54
mattpettenato <mattpettenato@gmail.com>
2021-12-06 02:21:33 -0800

    heroku

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+// keys_dev.js
+module.exports = {
+  mongoURI: 'mongodb+srv://demo:C1ZKMNExMeVjeZtU@cluster0.3xelk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+}
